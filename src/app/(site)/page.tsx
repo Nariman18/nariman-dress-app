7e0db9d17b9b3284f2f452550a1a60af175551bd
Nariman <Nariman@MacBook-Pro--Nariman.local>
2023-10-27 21:00:32 +0400

    first ecommerce-app

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -19,8 +19,6 @@ interface Props {
 }
 
 export default async function Home({ searchParams }: Props) {
-  const products = await getProducts(searchParams);
-
   return (
     <div>
       <div className="px-4 pt-20 text-center">
@@ -34,9 +32,7 @@ export default async function Home({ searchParams }: Props) {
       <div>
         <main className="mx-auto max-w-6xl px-6">
           <div className="flex items-center justify-between border-b border-gray-200 pb-4 pt-24 dark:border-gray-800">
-            <h1 className="text-xl font-bold tracking-tight sm:text-2xl">
-              {products.length} product{products.length === 1 ? "" : "s"}
-            </h1>
+            <h1 className="text-xl font-bold tracking-tight sm:text-2xl"></h1>
             {/* Product Sort */}
             <ProductSort />
           </div>
@@ -45,14 +41,7 @@ export default async function Home({ searchParams }: Props) {
             <h2 id="products-heading" className="sr-only">
               Products
             </h2>
-            <div
-              className={cn(
-                "grid grid-cols-1 gap-x-8 gap-y-10",
-                products.length > 0
-                  ? "lg:grid-cols-4"
-                  : "lg:grid-cols-[1fr_3fr]"
-              )}
-            >
+            <div className={cn("grid grid-cols-1 gap-x-8 gap-y-10")}>
               <div className="hidden lg:block">
                 {/* Product filters */}
                 <ProductFilters />
