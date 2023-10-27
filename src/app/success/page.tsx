9dbdef5651c0ae83056711557a1942c7e8cce7bd
Nariman <Nariman@MacBook-Pro--Nariman.local>
2023-10-27 17:43:39 +0400

    first ecommerce-app

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -3,7 +3,17 @@ import Link from "next/link";
 import { stripe } from "@/lib/stripe";
 import { CheckoutSession } from "@/components/CheckoutSession";
 
-export default async function Page() {
+interface Props {
+  searchParams: {
+    session_id?: string;
+  };
+}
+
+export default async function Page({ searchParams }: Props) {
+  const sessionId = searchParams?.session_id ?? "";
+  const checkoutSession = await stripe.checkout.sessions.retrieve(sessionId);
+  const customerDetails = checkoutSession.customer_details;
+
   return (
     <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
       <div className="text-center">
