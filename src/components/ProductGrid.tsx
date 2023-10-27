a8306320fde46b132ffcd7182b9bd76845795e2b
Nariman <Nariman@MacBook-Pro--Nariman.local>
2023-10-27 18:57:25 +0400

    first ecommerce-app

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -11,23 +11,12 @@ import { urlForImage } from "../../sanity/lib/image";
 import { useEffect, useState } from "react";
 import { getProducts } from "../../sanity/sanity-utils";
 
-interface Props {
-  searchParams: {
-    date?: string;
-    price?: string;
-    color?: string;
-    category?: string;
-    size?: string;
-    search?: string;
-  };
-}
-
-export function ProductGrid({ searchParams }: Props) {
+export function ProductGrid() {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const productData = await getProducts(searchParams);
+      const productData = await getProducts();
       const sortProductPage = productData.sort(
         (b, a) =>
           new Date(b._created_at).getTime() - new Date(a._created_at).getTime()
@@ -35,7 +24,7 @@ export function ProductGrid({ searchParams }: Props) {
       setProducts(sortProductPage);
     };
     fetchData();
-  }, [searchParams]);
+  }, []);
 
   if (products.length === 0) {
     return (
